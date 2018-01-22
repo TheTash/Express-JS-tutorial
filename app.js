0fad49cfd07e9568f07e542c3bbe86e61ac3ad76
TheTash <t.kashi.92@gmail.com>
2018-01-22 04:09:19 +0000

    renamed app file

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ var app = require('express')()
 var posts = [
   {
     id: 1,
-    author: 'John',
+    author: 'Tashi',
     title: 'Templating with EJS',
     body: 'Blog post number 1'
   },
   {
     id: 2,
-    author: 'Drake',
+    author: 'Misty',
     title: 'Express: Starting from the Bottom',
     body: 'Blog post number 2'
   },
   {
     id: 3,
-    author: 'Emma',
+    author: 'Gary',
     title: 'Streams',
     body: 'Blog post number 3'
   },
   {
     id: 4,
-    author: 'Cody',
+    author: 'Ash',
     title: 'Events',
     body: 'Blog post number 4'
   }
@@ -37,6 +37,7 @@ app.get('/post/:id', function (req, res) {
   var post = posts.filter(function (post) {
     return post.id == req.params.id;
   })[0];
+
   res.render('post', {
     author: post.author,
     title: post.title,
